af6f7119430ce9d8b44f4cd02d5b3dd1d4af9684
Shane Mindreau <smindre1@binghamton.edu>
2024-02-17 10:51:41 -0500

    Inital commit. Empty React.js setup.

---
+++ ReactSalon/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [language, setLanguage] = useState('english')
+
+  return (
+    <>
+      <h1>Salon Project Website</h1>
+    </>
+  )
+}
+
+export default App
