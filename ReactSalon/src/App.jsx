2fa7049e84918dc44c4ffee92430131d9d54ff06
Shane Mindreau <smindre1@binghamton.edu>
2024-02-19 20:46:57 -0500

    I created a basic footer for the website.

--- ReactSalon/src/App.jsx
+++ ReactSalon/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { Outlet } from 'react-router-dom';
 
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
