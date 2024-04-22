29014e659374977604e04b57252db9640eea9f4c
Shane Mindreau <smindre1@binghamton.edu>
2024-04-21 21:33:01 -0400

    I updated the /client/App.jsx file by adding ApolloClient and making a connection to graphql. I then wrapped the client in an ApolloProvider. This should allow for graphql requests as well as check for authorization when needed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,40 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext } from '@apollo/client/link/context';
 // require('dotenv').config();
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+
+const authLink1 = setContext((_,{headers}) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+})
+
+const client = new ApolloClient({
+  link: authLink1.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   
   return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
