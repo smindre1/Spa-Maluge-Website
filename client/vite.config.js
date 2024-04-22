eb58ca6c4ce8a76110ac625337e32ab6bedebe19
Shane Mindreau <smindre1@binghamton.edu>
2024-04-21 22:57:51 -0400

    updated vite.config to switch to server's port.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'https://spamaluge.com',
+        target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true
       }
