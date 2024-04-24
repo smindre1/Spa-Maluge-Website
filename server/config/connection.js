218ec55d84ece3e5e302282bf4ca81770f19ffd1
Shane Mindreau <smindre1@binghamton.edu>
2024-04-24 10:53:29 -0400

    Perhaps the error message was from the server side.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: "../../client/env" });
+require('dotenv').config();
 const mongoose = require('mongoose');
 console.log("connection");
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SpaMalugeReservationSystem');
