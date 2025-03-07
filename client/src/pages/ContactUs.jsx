99a95360e925a95cb1eb02b3e1ea6a1ffef88324
Shane Mindreau <smindre1@binghamton.edu>
2025-03-06 20:29:55 -0500

    Made a quick alteration to the address in the contact us page.

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -9,7 +9,7 @@ function ContactUs() {
           {/* <h2 className="minorTitle">Business Contacts</h2> */}
           <p className='businessInfoTxt'>(917) 921-4237</p>
           <p className='businessInfoTxt'>malugemc@gmail.com</p>
-          <a className='businessInfoTxt'>108 Adams St, Hoboken, NJ 07030</a>
+          <a className='businessInfoTxt'>108 Adams Street, B, Hoboken, NJ 07030</a>
         </div>
         <Form></Form>
       </section>
